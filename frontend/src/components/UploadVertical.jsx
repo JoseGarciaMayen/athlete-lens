8fb9396c1f17bc62ec157259802e21f18387a4a6
JoseGarciaMayen <josegarciamayen@gmail.com>
2026-06-12 15:35:40 +0200

    style(frontend): custom Tailwind styling for file inputs

--- frontend/src/components/UploadVertical.jsx
+++ frontend/src/components/UploadVertical.jsx
@@ -56,7 +56,13 @@ function UploadVertical() {
                         accept="video/*"
                         required
                         onChange={(e) => setFile(e.target.files[0])}
-                        className="block w-full"
+                        className="block w-full text-sm text-gray-500
+                            file:mr-4 file:py-2 file:px-4
+                            file:rounded-md file:border-0
+                            file:text-sm file:font-semibold
+                            file:bg-blue-50 file:text-blue-700
+                            hover:file:bg-blue-100
+                            cursor:pointer"
                     />
                 </div>
 
